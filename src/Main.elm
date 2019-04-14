59f37236aec5f54886ad7e3bc8887a717598e66e
Mariano Mariano <marianofheller@gmail.com>
2019-04-14 19:42:43 -0300

    init

--- src/Main.elm
+++ src/Main.elm
@@ -1,97 +1,196 @@
 module Main exposing (main)
 
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta)
+import Browser.Dom exposing (Viewport, getViewport)
+import Browser.Events exposing (onAnimationFrameDelta, onResize)
 import Canvas exposing (..)
 import Color
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (style)
-import Platform exposing (..)
-import Time
 import Particle as P
+import Platform exposing (..)
+import Task
+import Time
 
 
-type alias Model = {
-    frameCount : Float,
-    fps : Float,
-    particles: List P.Particle
-  }
+type alias ViewportInfo =
+    { width : Float
+    , height : Float
+    }
+
+
+toViewPortInfo : Viewport -> ViewportInfo
+toViewPortInfo v =
+    { width = v.viewport.width, height = v.viewport.height }
+
+
+type alias MetaInfo =
+    { frameCount : Float
+    , frameCountAcc : Float
+    , fps : Float
+    , viewport : ViewportInfo
+    }
+
+
+type alias Model =
+    { metaInfo : MetaInfo
+    , particles : List P.Particle
+    }
 
 
 type Msg
-    = Frame Float | Tick Time.Posix
+    = Frame Float
+    | Tick Time.Posix
+    | ViewportInfoUpdate ViewportInfo
+
+
+setMetaInfo : MetaInfo -> Model -> Model
+setMetaInfo f m =
+    { m | metaInfo = f }
+
+
+setFrameCount : Float -> MetaInfo -> MetaInfo
+setFrameCount c f =
+    { f | frameCount = c }
+
+increaseFrameCount : MetaInfo -> MetaInfo
+increaseFrameCount f =
+    { f | frameCount = f.frameCount + 1, frameCountAcc = f.frameCount + 1 }
+
+
+setViewportInfo : ViewportInfo -> MetaInfo -> MetaInfo
+setViewportInfo vInfo m =
+    { m | viewport = vInfo }
+
+
+calcFps : MetaInfo -> MetaInfo
+calcFps f =
+    { f | frameCount = 0, fps = f.frameCount }
 
 
 main : Program () Model Msg
-main = Browser.element {
-      init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-  }
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-init _ = (
-    { frameCount = 0, fps = 0, particles = [] },
-    Cmd.none
-  )
 
-update msg model = 
-  case msg of
-    Frame _ ->
-        ( { model | frameCount = model.frameCount + 1 }, Cmd.none )
-    Tick _ -> ({ model | frameCount = 0, fps = model.frameCount }, Cmd.none)
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { metaInfo =
+            { frameCount = 0
+            , frameCountAcc = 0
+            , fps = 50
+            , viewport =
+                { width = 0
+                , height = 0
+                }
+            }
+      , particles = []
+      }
+    , getViewport |> Task.perform (\v -> ViewportInfoUpdate (toViewPortInfo v))
+    )
+
+
+update msg model =
+    case msg of
+        ViewportInfoUpdate viewport ->
+            let
+                newModel =
+                    model
+                        |> setMetaInfo (setViewportInfo viewport model.metaInfo)
+            in
+            ( newModel, Cmd.none )
+
+        Frame _ ->
+            let
+                newModel =
+                    model
+                        |> setMetaInfo (increaseFrameCount model.metaInfo)
+            in
+            ( newModel, Cmd.none )
+
+        Tick _ ->
+            let
+                newModel =
+                    model
+                        |> setMetaInfo (calcFps model.metaInfo)
+            in
+            ( newModel, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch [
-      onAnimationFrameDelta Frame,
-      Time.every 1000 Tick
-    ]
+    Sub.batch
+        [ onAnimationFrameDelta Frame
+        , onResize (\w h -> ViewportInfoUpdate { width = toFloat w, height = toFloat h })
+        , Time.every 1000 Tick
+        ]
 
-width : number
-width = 400
-height : number
-height = 400
-
-
-centerX = width / 2
-centerY = height / 2
 
 view : Model -> Html Msg
-view { frameCount, fps } =
+view { metaInfo } =
     div
         [ style "display" "flex"
         , style "justify-content" "center"
         , style "align-items" "center"
         ]
         [ Canvas.toHtml
-            ( width, height )
-            [ style "border" "10px solid rgba(0,0,0,0.1)" ]
-            [ clearScreen
-            , render frameCount
+            ( round metaInfo.viewport.width, round metaInfo.viewport.height )
+            [ style "border" "5px solid rgba(0,0,0,0.1)" ]
+            [ clearScreen metaInfo.viewport
+            , render metaInfo.frameCountAcc metaInfo.viewport
             ]
-        , text <| String.fromFloat fps
+        , viewFPS metaInfo.fps
         ]
 
+viewFPS fps =
+    div
+        [ style "position" "absolute"
+        , style "top" "1rem"
+        , style "right" "1rem"
+        , style "z-index" "10"
+        ]
+        [ text <| String.fromFloat fps ]
 
-clearScreen =
-    shapes [ fill Color.white ] [ rect ( 0, 0 ) width height ]
+
+clearScreen : ViewportInfo -> Renderable
+clearScreen v =
+    shapes [ fill Color.black ] [ rect ( 0, 0 ) v.width v.height ]
 
 
-render frameCount =
+render : Float -> ViewportInfo -> Renderable
+render count v =
     let
         size =
-            width / 3
+            v.height / 3
+        
+        centerX =
+            v.width / 2
+        
+        centerY =
+            v.height / 2
+
         x =
-            -(size / 2)
+            centerX - (size / 2)
+
         y =
-            -(size / 2)
+            centerY - (size / 2)
+            
+        rotation =
+            degrees (count * 3)
+
+        hue =
+            toFloat (count / 4 |> floor |> modBy 100) / 100
     in
     shapes
         [ transform
             [ translate centerX centerY
-            , rotate (degrees (frameCount * 3))
+            , rotate rotation
             ]
-        , fill (Color.hsl (degrees (frameCount / 4)) 0.3 0.7)
+        , fill (Color.hsl hue 0.3 0.7)
         ]
         [ rect ( x, y ) size size ]
