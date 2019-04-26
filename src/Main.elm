9f633c14c284b2fb1137696784fcf318c375ca80
Mariano Heller <mariano.heller@w3americas.com>
2019-04-25 22:43:47 -0300

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -15,6 +15,20 @@ import Platform exposing (..)
 import Task
 import Time
 
+{- GRAVITY -}
+g : { x : Float, y : Float }
+g = { x = 0, y = -9.8 }
+
+{- PARTICLE MASS -}
+mass : Float
+mass = 5
+
+{- DRAG COEFF TOTAL-}
+{- Drag = Cd * .5 * r * V^2 * A -}
+{- Cd: drag coeff, r: air density -}
+{- Cd * .5 * r -}
+d : Float
+d = 1
 
 type alias ViewportInfo =
     { width : Float
@@ -31,6 +45,7 @@ type alias MetaInfo =
     { frameCount : Float
     , frameCountAcc : Float
     , fps : Float
+    , startTime : Int
     , viewport : ViewportInfo
     }
 
@@ -43,8 +58,16 @@ type alias Model =
 
 type Msg
     = Frame Float
-    | Tick Time.Posix
+    | Tick1s Time.Posix
+    | Tick17ms Time.Posix
     | ViewportInfoUpdate ViewportInfo
+    | InitParticlePositions ViewportInfo
+    | InitStartTime Time.Posix
+
+
+setParticles : List P.Particle -> Model -> Model
+setParticles ps m =
+    { m | particles = ps }
 
 
 setMetaInfo : MetaInfo -> Model -> Model
@@ -55,6 +78,11 @@ setMetaInfo f m =
 setFrameCount : Float -> MetaInfo -> MetaInfo
 setFrameCount c f =
     { f | frameCount = c }
+
+
+setStartTime : Int -> MetaInfo -> MetaInfo
+setStartTime st f =
+    { f | startTime = st }
 
 
 increaseFrameCount : MetaInfo -> MetaInfo
@@ -87,15 +115,20 @@ init _ =
     ( { metaInfo =
             { frameCount = 0
             , frameCountAcc = 0
-            , fps = 50
+            , fps = 60
+            , startTime = 0
             , viewport =
                 { width = 0
                 , height = 0
                 }
             }
-      , particles = range 0 100 |> map (\x -> P.createParticle { x = toFloat x, y = 0 })
+      , particles = range 0 9 |> map (\x -> P.createParticle { x = toFloat x, y = 0 } mass)
       }
-    , getViewport |> Task.perform (\v -> ViewportInfoUpdate (toViewPortInfo v))
+    , Cmd.batch
+        [ getViewport |> Task.perform (\v -> ViewportInfoUpdate (toViewPortInfo v))
+        , getViewport |> Task.perform (\v -> InitParticlePositions (toViewPortInfo v))
+        , Time.now  |> Task.perform (\t -> InitStartTime t)
+        ]
     )
 
 
@@ -109,6 +142,22 @@ update msg model =
             in
             ( newModel, Cmd.none )
 
+        InitParticlePositions viewport ->
+            let
+                widthP = viewport.width / 10
+
+                heightP = viewport.height / 10
+
+                newModel =
+                    model
+                        |> setParticles
+                            (map
+                                (\p -> P.setPos { x = p.p.x * widthP, y = heightP } p)
+                                model.particles
+                            )
+            in
+            ( newModel, Cmd.none )
+
         Frame _ ->
             let
                 newModel =
@@ -117,11 +166,30 @@ update msg model =
             in
             ( newModel, Cmd.none )
 
-        Tick _ ->
+        InitStartTime pTime ->
+            let
+                newModel =
+                    model
+                        |> setMetaInfo (setStartTime (Time.posixToMillis pTime) model.metaInfo)
+            in
+            ( newModel, Cmd.none )
+
+        Tick1s _ ->
             let
                 newModel =
                     model
                         |> setMetaInfo (calcFps model.metaInfo)
+            in
+            ( newModel, Cmd.none )
+
+        Tick17ms pTime ->
+            let
+                newModel =
+                    model
+                        |> setParticles (map
+                            (P.nextTick g d ((Time.posixToMillis pTime) - model.metaInfo.startTime))
+                            model.particles
+                        )
             in
             ( newModel, Cmd.none )
 
@@ -131,7 +199,8 @@ subscriptions model =
     Sub.batch
         [ onAnimationFrameDelta Frame
         , onResize (\w h -> ViewportInfoUpdate { width = toFloat w, height = toFloat h })
-        , Time.every 1000 Tick
+        , Time.every 1000 Tick1s
+        , Time.every 17 Tick17ms
         ]
 
 
@@ -159,27 +228,34 @@ viewFPS fps =
 
 viewSvg : Model -> Html Msg
 viewSvg model =
-    renderBackground model.metaInfo.viewport
-        |> at topLeft (renderSquare model.metaInfo.frameCountAcc model.metaInfo.viewport)
-        |> at topLeft (renderParticles model.particles)
+    impose
+        {- (renderSquare model.metaInfo.frameCountAcc model.metaInfo.viewport) -} 
+        (renderParticles model.particles)
+        (renderBackground model.metaInfo |> align topLeft)
         |> svg
 
 
-renderBackground : ViewportInfo -> Collage msg
-renderBackground v =
-    rectangle v.width v.height
-        |> filled (uniform Color.white)
+renderBackground : MetaInfo -> Collage msg
+renderBackground m =
+    let
+        hue =
+            toFloat (m.frameCountAcc / 8 |> floor |> modBy 100) / 100
+    in
+    rectangle m.viewport.width m.viewport.height
+        |> filled (uniform <| Color.hsl hue 0.1 0.9)
 
 
 drawParticle : P.Particle -> Collage msg
-drawParticle p = 
+drawParticle p =
     circle p.particleRadius
         |> filled (uniform Color.red)
+        |> shift (p.p.x, (-1) * p.p.y)
 
 
 renderParticles : List P.Particle -> Collage msg
-renderParticles ps = group <| map drawParticle ps
-    
+renderParticles ps =
+    group <| map drawParticle ps
+
 
 renderSquare : Float -> ViewportInfo -> Collage msg
 renderSquare count v =
@@ -206,6 +282,5 @@ renderSquare count v =
             toFloat (count / 4 |> floor |> modBy 100) / 100
     in
     rectangle size size
-        |> filled (uniform <| Color.hsl hue 0.3 0.7)
-        |> shift ( centerX, centerY )
+        |> filled (uniform <| Color.hsl hue 0.45 0.7)
         |> rotate rotation
--- src/Particle.elm
+++ src/Particle.elm
@@ -1,61 +1,125 @@
-module Particle exposing (Particle, createParticle)
-
-type alias TupleXY = { x : Float, y : Float }
-
-type alias Tick = Float
-
-type alias Particle = {
-    v : TupleXY,
-    p : TupleXY,
-    collisionRadius : Float,
-    particleRadius : Float
-  }
+module Particle exposing (Particle, createParticle, nextTick, setPos)
 
 
-{- Lenses??? -}
-setPos : Particle -> TupleXY -> Particle 
-setPos e p = { e | p = p }
-
-setVel : Particle -> TupleXY -> Particle 
-setVel e v = { e | v = v }
+type alias TupleXY =
+    { x : Float, y : Float }
 
 
-createParticle : TupleXY -> Particle
-createParticle pos = {
-    p = pos,
-    v = {
-      x = 0.0,
-      y = 0.0
-    },
-    collisionRadius = 5,
-    particleRadius = 5
-  }
-
-nextTick : Particle -> TupleXY -> Tick -> Particle
-nextTick e a t = { e |
-    p = nextPosition e t,
-    v = nextVelocity e a t
-  }
-
-nextPosition : Particle -> Tick -> TupleXY
-nextPosition e t = {
-    x = (e.v.x * t + e.p.x),
-    y = (e.v.y * t + e.p.y)
-  }
+type alias Tick =
+    Int
 
 
-nextVelocity : Particle -> TupleXY -> Tick -> TupleXY
-nextVelocity e a t = {
-    x = (a.x * t + e.v.x),
-    y = (a.y * t + e.v.y)
-  }
-
-{- 
-
-detectCollision : List Particle -> Particle -> Boolean
-detectCollision (a:as) b = collide a b || detectCollision as b
+type alias Particle =
+    { p : TupleXY
+    , v : TupleXY
+    , a : TupleXY
+    , collisionRadius : Float
+    , particleRadius : Float
+    , mass : Float
+    }
 
 
-collide : Particle -> Particle -> Boolean
-collide e1 e2 = d > sqrt ((e1.p.x * e2.p.x) + (e1.p.y * e2.p.y))
-  where d = e1.collisionRadius + e2.collisionRadius -}
+setPos : TupleXY -> Particle -> Particle
+setPos p e =
+    { e | p = p }
+
+
+setVel : Particle -> TupleXY -> Particle
+setVel e v =
+    { e | v = v }
+
+
+setAccel : Particle -> TupleXY -> Particle
+setAccel e a =
+    { e | a = a }
+
+
+createParticle : TupleXY -> Float -> Particle
+createParticle pos mass =
+    { p = pos
+    , a =
+        { x = 0.0
+        , y = 0.0
+        }
+    , v =
+        { x = 0.0
+        , y = 0.0
+        }
+    , collisionRadius = 5
+    , particleRadius = 5
+    , mass = mass
+    }
+
+
+nextTick : TupleXY -> Float -> Tick -> Particle -> Particle
+nextTick g dCoeff t e =
+    let
+        surface =
+            e.particleRadius * 2 * 3.14
+
+        dragX =
+            dCoeff * (e.v.x ^ 2) * surface
+
+        dragY =
+            dCoeff * (e.v.y ^ 2) * surface
+
+        weightX =
+            e.mass * (g.x + e.a.x)
+
+        weightY =
+            e.mass * (g.y + e.a.y)
+
+        wxdx =
+            if weightX - dragX < 0 then
+                0
+
+            else
+                dragX - weightX
+
+        wydy =
+            if abs weightY - abs dragY < 0 then
+                0
+
+            else
+                dragY - weightY
+
+        a =
+            { x = wxdx / e.mass
+            , y = wydy / e.mass
+            }
+    in
+    { e
+        | p = nextPosition t e
+        , v = nextVelocity a t e
+        , a =
+            { x = a.x - g.x
+            , y = a.y - g.y
+            }
+    }
+
+
+nextPosition : Tick -> Particle -> TupleXY
+nextPosition t e =
+    { x = e.v.x * (toFloat t / 1000) + e.p.x
+    , y = e.v.y * (toFloat t / 1000) + e.p.y
+    }
+
+
+nextVelocity : TupleXY -> Tick -> Particle -> TupleXY
+nextVelocity a t e =
+    { x = a.x * (toFloat t / 1000) + e.v.x
+    , y = a.y * (toFloat t / 1000) + e.v.y
+    }
+
+
+
+{-
+
+   detectCollision : List Particle -> Particle -> Boolean
+   detectCollision (a:as) b = collide a b || detectCollision as b
+
+
+   collide : Particle -> Particle -> Boolean
+   collide e1 e2 = d > sqrt ((e1.p.x * e2.p.x) + (e1.p.y * e2.p.y))
+     where d = e1.collisionRadius + e2.collisionRadius
+-}
